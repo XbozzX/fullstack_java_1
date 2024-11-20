c4a68ad83a91f35bbd7c11ca9484514caa0029af
aqey <aqilkamel49@gmail.com>
2024-11-20 17:00:55 +0800

    KAN-7 <encapsulation>

--- ccsd/src/main/java/com/example/ccsd/WebsiteImages/WebsiteImages.java
+++ ccsd/src/main/java/com/example/ccsd/WebsiteImages/WebsiteImages.java
@@ -1,5 +1,22 @@
 package com.example.ccsd.WebsiteImages;
 
+import java.util.Date;
+
 public class WebsiteImages {
     
+    private String image;
+    private Date uploadDate;
+    
+
+public String getimage() {
+    return image;
+} 
+
+public Date getuploadDate(){
+    return uploadDate;
+}
+
+
+
+
 }
