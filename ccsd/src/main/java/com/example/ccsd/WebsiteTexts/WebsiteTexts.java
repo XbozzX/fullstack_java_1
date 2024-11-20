135f82f48f510ba414d53603f01a366f83174646
nuraimifarhana <nuraimifarhana58@gmail.com>
2024-11-20 17:01:47 +0800

    KAN-8 <update website text encapsulation>

--- ccsd/src/main/java/com/example/ccsd/WebsiteTexts/WebsiteTexts.java
+++ ccsd/src/main/java/com/example/ccsd/WebsiteTexts/WebsiteTexts.java
@@ -1,5 +1,37 @@
 package com.example.ccsd.WebsiteTexts;
 
+
 public class WebsiteTexts {
+
+  private String text;
+  private String description;
+  private String caption;
+  private String title;
+  private String subtitle;
+  private String about;
+
+  public String getText(){
+    return text;
+  }
+  public String getDescription(){
+    return description;
+  }
+  public String getCaption(){
+    return caption;
+  }
+  public String getTitle(){
+    return title;
+  }
+  public String getSubtitle(){
+    return subtitle;
+  }
+  public String getAbout(){
+    return about;
+  }
+    public static void main(String[]args) throws IOException{}
+    
+  
+
+   
     
 }
