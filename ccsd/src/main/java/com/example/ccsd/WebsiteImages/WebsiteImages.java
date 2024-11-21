2328da7562c4c831e29c23136ba5d993af2d6239
aqey <aqilkamel49@gmail.com>
2024-11-21 12:21:02 +0800

    KAN-7 <second>

--- ccsd/src/main/java/com/example/ccsd/WebsiteImages/WebsiteImages.java
+++ ccsd/src/main/java/com/example/ccsd/WebsiteImages/WebsiteImages.java
@@ -2,21 +2,69 @@ package com.example.ccsd.WebsiteImages;
 
 import java.util.Date;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "WebsiteImages")
 public class WebsiteImages {
     
+    @Id
+    private String id;
     private String image;
     private Date uploadDate;
+    private String galleryImage;
+    private String introductionImage;
     
+    
+    public WebsiteImages() {}
+
+    public WebsiteImages(String image, Date uploadDate, String galleryImage, String introductionImage) {
+        this.image = image;
+        this.uploadDate = uploadDate;
+        this.galleryImage = galleryImage;
+        this.introductionImage = introductionImage;
+    }
+
+public String getId(){
+    return id;
+}
+
+public void setId(String id) {
+    this.id = id;
+}
+
 
 public String getimage() {
     return image;
 } 
 
+public void setImage(String image) {
+    this.image = image;
+}
+
+
 public Date getuploadDate(){
     return uploadDate;
 }
 
+public void setuploadDate(Date uploadDate) {
+    this.uploadDate = uploadDate;
+}
 
+public String getgalleryImage(){
+    return galleryImage;
+}
 
+public void setgalleryImage(String galleryImage) {
+    this.galleryImage = galleryImage;
+}
+
+public String getintroductionImage(){
+    return introductionImage;
+}
+
+public void setintroductionImage(String introductionImage) {
+    this.introductionImage = introductionImage;
+}
 
 }
