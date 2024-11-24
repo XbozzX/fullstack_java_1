6e2aea39dde01d878273b4bf390d5c064edddcd2
XbozzX <afham821@gmail.com>
2024-11-25 02:26:25 +0800

    KAN-13 <update the payment page>

--- dashboard/src/data/getData.js
+++ dashboard/src/data/getData.js
@@ -118,7 +118,7 @@ const GetData = {
         {
           // request headers
           headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json', 
             Authorization: `Bearer ${token}`,
           }
         }
@@ -208,21 +208,21 @@ const GetData = {
     
   },
 
-  async getProduct(slug) {
-    const token = await localStorage.getItem('jwtToken');
+  async getProduct(id) {
+
 
     try {
         const response = await axios.get(
-            `${API_BASE_URL}/api/product/${slug}`,
+            `${API_BASE_URL}/api/products/${id}`,
             {
                 // request headers
                 headers: {
                     'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`,
+    
                 }
             }
         );
-          
+
         if (response.status === 200) {
             return response.data;
         }
