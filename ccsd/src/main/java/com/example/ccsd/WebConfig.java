144c8ff6809a67afc19ea9be0094e19edde0cad9
XbozzX <afham821@gmail.com>
2026-01-04 03:27:54 +0800

    update mongo DB from local to cloud

--- ccsd/src/main/java/com/example/ccsd/WebConfig.java
+++ ccsd/src/main/java/com/example/ccsd/WebConfig.java
@@ -1,12 +1,20 @@
 package com.example.ccsd;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+
 @Configuration
 public class WebConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -16,18 +24,23 @@ public class WebConfig {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow OPTIONS method
-                        .allowedHeaders("*")  // Allow all headers
+                        .allowedHeaders("*") // Allow all headers
                         .exposedHeaders("Authorization") // In case you need to expose any headers
                         .allowCredentials(false) // In case you're not using cookies or authentication
                         .maxAge(3600); // Cache the preflight request for 1 hour
             }
-            
+
             @Override
             public void addResourceHandlers(ResourceHandlerRegistry registry) {
                 registry.addResourceHandler("/images/**")
                         .addResourceLocations("classpath:/static/images/");
-    }
+            }
         };
+    }
+    
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(uri);
     }
 }
 
