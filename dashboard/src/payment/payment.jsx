5f9bc034f0d3f560985c3b03347a31c48ed848b5
XbozzX <afham821@gmail.com>
2026-02-13 07:59:06 +0800

    re-deploy

--- dashboard/src/payment/payment.jsx
+++ dashboard/src/payment/payment.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import "./payment.css";
-import GetData from "../data/getData";
 
 const Payment = () => {
   const { productSlug } = useParams();
